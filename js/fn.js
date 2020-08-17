92ff9fbdeab4ff5d6a6ed5d820228d3157239082
Franklin Chinedu <35113848+Nobledsmarts@users.noreply.github.com>
2020-08-17 16:35:15 +0000

    Delete fn.js

--- js/fn.js
+++
@@ -1,8 +0,0 @@
-function showLoader(){
-    document.getElementsByClassName('loader-container')[0].style.display = 'flex';
-    document.getElementsByClassName('app')[0].style.display = 'none';
-}
-function hideLoader(){
-    document.getElementsByClassName('loader-container')[0].style.display = 'none';
-    document.getElementsByClassName('app')[0].style.display = 'block';
-}
